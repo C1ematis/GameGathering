f6bf2febee73842ccd4b63c0acf32c08b58c2439
Clematis <khtafa@gmail.com>
2021-10-19 15:17:48 +0000

    Fix:オートコンプリート

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,72 +1,72 @@
-  $(function() {
-    const dataListTitle = function(request, response) {
-      $.ajax({
-        url: '/autocomplete_games/' + extractLast( request.term ),
-        dataType: 'json',
-        type: 'GET',
-        cache: true,
-        success: function(data) {
-          response(data);
-        },
-        error: function(XMLHttpRequest, textStatus, errorThrown) {
-          response(['']);
-        }
-      });
-    }
-    const dataListTag = function(request, response) {
-      $.ajax({
-        url: '/autocomplete_tags/' + extractLast( request.term ),
-        dataType: 'json',
-        type: 'GET',
-        cache: true,
-        success: function(data) {
-          response(data);
-        },
-        error: function(XMLHttpRequest, textStatus, errorThrown) {
-          response(['']);
-        }
-      });
-    }
-    function split( val ) {
-      return val.split( /\s/ );
-    }
-    function extractLast( term ) {
-      return split( term ).pop();
-    }
-    $('.autocomplete-title').autocomplete({
-      source: dataListTitle,
-      delay: 300, // 入力してからサジェストが動くまでの時間(ms)
-      search: function() {
-        var term = extractLast( this.value );
-        if ( term.length < 1 ) {
-          return false;
-        }
+$(document).on('turbolinks:load', function () {
+  const dataListTitle = function(request, response) {
+    $.ajax({
+      url: '/autocomplete_games/' + extractLast( request.term ),
+      dataType: 'json',
+      type: 'GET',
+      cache: true,
+      success: function(data) {
+        response(data);
       },
-      select: function( event, ui ) {
-        var terms = split( this.value );
-        terms.pop();
-        terms.push( ui.item.value );
-        terms.push( "" );
-        this.value = terms.join( " " );
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        response(['']);
+      }
+    });
+  }
+  const dataListTag = function(request, response) {
+    $.ajax({
+      url: '/autocomplete_tags/' + extractLast( request.term ),
+      dataType: 'json',
+      type: 'GET',
+      cache: true,
+      success: function(data) {
+        response(data);
+      },
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        response(['']);
+      }
+    });
+  }
+  function split( val ) {
+    return val.split( /\s/ );
+  }
+  function extractLast( term ) {
+    return split( term ).pop();
+  }
+  $('.autocomplete-title').autocomplete({
+    source: dataListTitle,
+    delay: 300,
+    search: function() {
+      var term = extractLast( this.value );
+      if ( term.length < 1 ) {
         return false;
       }
-    })
-    $('.autocomplete-tag').autocomplete({
-      source: dataListTag,
-      delay: 300, // 入力してからサジェストが動くまでの時間(ms)
-      search: function() {
-        var term = extractLast( this.value );
-        if ( term.length < 1 ) {
-          return false;
-        }
-      },
-      select: function( event, ui ) {
-        var terms = split( this.value );
-        terms.pop();
-        terms.push( ui.item.value );
-        terms.push( "" );
-        this.value = terms.join( " " );
+    },
+    select: function( event, ui ) {
+      var terms = split( this.value );
+      terms.pop();
+      terms.push( ui.item.value );
+      terms.push( "" );
+      this.value = terms.join( " " );
+      return false;
+    }
+  })
+  $('.autocomplete-tag').autocomplete({
+    source: dataListTag,
+    delay: 300,
+    search: function() {
+      var term = extractLast( this.value );
+      if ( term.length < 1 ) {
         return false;
       }
-    })
-  });
+    },
+    select: function( event, ui ) {
+      var terms = split( this.value );
+      terms.pop();
+      terms.push( ui.item.value );
+      terms.push( "" );
+      this.value = terms.join( " " );
+      return false;
+    }
+  })
+});
